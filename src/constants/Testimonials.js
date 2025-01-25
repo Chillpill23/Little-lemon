4cb3ffca068d6ea746ad4a776beaeb9c37d15358
Mark Lagarta <mj,lagarta00@gmail.com>
2025-01-25 14:35:31 +0800

    Adding multiple assets and components for site development

---
+++ src/constants/Testimonials.js
@@ -0,0 +1,22 @@
+import avatar from "../images/avatar.jpg"
+
+export const Testimonials = [
+  {
+    rating: 5,
+    img:avatar,
+    name: 'Daryl',
+    review: 'The food was awesome'
+  },
+  {
+    rating: 4,
+    img:avatar,
+    name: 'Rick',
+    review: 'Very delicious'
+  },
+  {
+    rating: 5,
+    img:avatar,
+    name: 'Michone',
+    review: 'Truly great!'
+  },
+]
