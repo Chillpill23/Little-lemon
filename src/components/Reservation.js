242a358c1b660659730de962890b4a3bc20ecc1b
Mark Lagarta <mj,lagarta00@gmail.com>
2025-01-31 12:20:20 +0800

    Added unit testing for Table Booking feature reducer functions

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,9 +1,37 @@
 import BookingForms from "./BookingForms";
+import { useReducer } from "react";
+
+export const initializeTimes = () =>{
+  return [  
+    '17:00', 
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00'
+  ]
+}
+
+export const availableTimesReducer = (state, action) => {
+  switch (action.type) {
+    case "set_times":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 const Reservation = () =>{
+
+  const [availableTime, dispatch] = useReducer(availableTimesReducer, initializeTimes())
+  
+  const updateTimes = (newTimes) =>{
+    dispatch({type:'set_times', payload: newTimes})
+  }
+
   return(
     <>
-      <BookingForms />
+      <BookingForms availableTime={availableTime} updateAvailableTime={updateTimes}/>
     </>
   )
 }
